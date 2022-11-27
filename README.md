c1fce12a7e716ca1f92481d97bee96f79b44e14e
Benedikt Böhm <bb@xnull.de>
2022-11-27 12:36:23 +0100

    Initialize with copier

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# ansible-role-zfs
-Install ZFS repository and packages with Ansible.
+# Ansible Role for ZFS
+
+Install and configure ZFS kernel modules and pools with Ansible.
+
+The [Contributing Guide](CONTRIBUTING.md) explains how to work with and
+contribute to this repository.
---
+++ python.mk
@@ -0,0 +1,14 @@
+.PHONY: python-install
+python-install: ## install Python packages from pyproject.toml
+	poetry install --no-interaction --no-root
+install:: python-install
+
+.PHONY: python-update
+python-update:: ## update Python packages from pyproject.toml
+	poetry update --no-interaction
+update:: python-update
+
+.PHONY: python-clean
+python-clean: ## remove current Python virtualenv
+	poetry env remove python
+clean:: python-clean
